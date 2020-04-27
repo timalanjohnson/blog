fa6b5d598a85ae1aeba9ffebcac2032c4a0fa9f6
Timothy Johnson <tim.johnson@yoco.com>
2020-04-27 17:55:36 +0200

    Draft functionality

---
+++ content/blog/test-draft/index.md
@@ -0,0 +1,8 @@
+---
+title: "Draft"
+date: "2020-04-26"
+description: "Draft."
+draft: true
+---
+
+This post should not be visible.
--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,7 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    `gatsby-plugin-draft`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
