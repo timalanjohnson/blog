f589d9bcd093be6e7299acd23988b166abd383e5
Timothy Johnson <tim.johnson@yoco.com>
2020-04-27 19:16:33 +0200

    theme color

--- src/html.js
+++ src/html.js
@@ -11,7 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <meta theme-color="#141414" />
+        <meta name="theme-color" content="#141414" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
